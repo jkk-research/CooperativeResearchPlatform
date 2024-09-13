ade46939d889ce0d1e0cab8eb19b35acf6376006
AnonymDavid <jodavid512@gmail.com>
2024-09-13 13:59:59 +0200

    Add Dockerfile and script for building docker

---
+++ docker/build_arm64.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -f "docker/Dockerfile" ]; then
+    echo "Please run the script from the root directory of the project"
+    exit 1
+fi
+
+docker build --platform linux/arm64 -t crp -f docker/Dockerfile .
