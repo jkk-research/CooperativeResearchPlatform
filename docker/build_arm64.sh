411415ff651b4d8511f8c14ebac9dd751f84ee62
AnonymDavid <jodavid512@gmail.com>
2024-09-16 15:39:13 +0200

    Updated docs, renamed docker container

--- docker/build_arm64.sh
+++ docker/build_arm64.sh
@@ -5,4 +5,4 @@ if [ ! -f "docker/Dockerfile" ]; then
     exit 1
 fi
 
-docker build --platform linux/arm64 -t crp -f docker/Dockerfile .
+docker build --platform linux/arm64 -t crp_arm64 -f docker/Dockerfile .
