390ffdd9ad349ce7db0e31e5a3c9af281fbd96a1
AnonymDavid <jodavid512@gmail.com>
2024-09-18 11:10:35 +0200

    Added map visualizer to launcher

--- crp_VIL/lanelet_handler/launch/laneletFileLoader.launch.py
+++ crp_VIL/lanelet_handler/launch/laneletFileLoader.launch.py
@@ -17,6 +17,12 @@ def generate_launch_description():
         description='Output topic for the lanelet2 map binary'
     )
 
+    lanelet2_visualization_topic_arg = DeclareLaunchArgument(
+        'map_visualization_topic',
+        default_value="/map/global_static_map_from_file/lanelet2_map_visualization",
+        description='Output topic for the lanelet2 map binary'
+    )
+
     lanelet2_map_frame_id_arg = DeclareLaunchArgument(
         'map_frame_id',
         default_value="map",
@@ -35,10 +41,23 @@ def generate_launch_description():
             'map_frame_id':     LaunchConfiguration('map_frame_id')
         }]
     )
+
+    lanelet_map_visualizer = Node(
+        package='map_loader',
+        executable='lanelet2_map_visualization',
+        name='lanelet2_map_visualization',
+        output='screen',
+        remappings=[
+            ('input/lanelet2_map', LaunchConfiguration('map_output_topic')),
+            ('output/lanelet2_map_marker', LaunchConfiguration('map_visualization_topic'))
+        ]
+    )
     
     return LaunchDescription([
         lanelet2_path_arg,
         lanelet2_output_topic_arg,
+        lanelet2_visualization_topic_arg,
         lanelet2_map_frame_id_arg,
-        lanelet_map_handler
+        lanelet_map_handler,
+        lanelet_map_visualizer
     ])
