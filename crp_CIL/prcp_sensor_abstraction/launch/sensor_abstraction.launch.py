6e53138b1de50ce7eac12b0c7d90af6d4c9174a0
AnonymDavid <jodavid512@gmail.com>
2024-10-07 14:24:01 +0200

    Add ego abstraction to launcher

--- crp_CIL/prcp_sensor_abstraction/launch/sensor_abstraction.launch.py
+++ crp_CIL/prcp_sensor_abstraction/launch/sensor_abstraction.launch.py
@@ -1,17 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
+    local_path_length_arg = DeclareLaunchArgument(
+        'local_path_length',
+        default_value='70.0',
+        description='Length of the scenario in meters'
+    )
+    
     scenario_abstraction = Node(
         package='prcp_sensor_abstraction',
         executable='scenario_abstraction',
         name='scenario_abstraction',
         output='screen',
-        parameters=[
-            {'local_path_length': 70.0}
-        ]
+        parameters=[{
+            'local_path_length': LaunchConfiguration('local_path_length')
+        }]
+    )
+    
+    ego_abstraction = Node(
+        package='prcp_sensor_abstraction',
+        executable='ego_abstraction',
+        name='ego_abstraction',
+        output='screen',
     )
 
     return LaunchDescription([
-        scenario_abstraction
+        local_path_length_arg,
+        scenario_abstraction,
+        ego_abstraction
     ])
