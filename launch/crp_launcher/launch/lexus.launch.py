465d265142d82572856b6d183f9faf2f31790a98
AnonymDavid <jodavid512@gmail.com>
2024-10-08 16:05:15 +0200

    Add launchers (WIP) and necessary submodules

---
+++ launch/crp_launcher/launch/lexus.launch.py
@@ -0,0 +1,113 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from os.path import join
+
+
+def generate_launch_description():
+    novatel_gps = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('novatel_gps_launcher'),
+                'launch',
+                'novatel.launch.py')
+        ),
+        launch_arguments={
+            'novatel_namespace': 'gps/nova',
+            'novatel_ip': '192.168.10.12',
+            'novatel_port': '3001',
+            'novatel_imu_frame_id': '/lexus3/nova/imu',
+            'novatel_frame_id': '/lexus3/gps/nova'
+        }.items()
+    )
+
+    static_tf = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('lexus_bringup'),
+                'launch',
+                'tf_static.launch.py')
+        )
+    )
+
+    vehicle_can = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            join(
+                get_package_share_directory('lexus_bringup'),
+                'launch',
+                'drivers',
+                'can_pacmod3.launch.xml')
+        )
+    )
+
+    sensor_abstraction = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('prcp_sensor_abstraction'),
+                'launch',
+                'sensor_abstraction.launch.py')
+        )
+    )
+
+    environmental_fusion = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('prcp_situation_analysis'),
+                'launch',
+                'environmental_fusion.launch.py')
+        )
+    )
+
+    behavior_planning = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('plan_behavior_planning'),
+                'launch',
+                'behavior_planning.launch.py'
+            )
+        )
+    )
+
+    motion_planning = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('plan_motion_planning'),
+                'launch',
+                'motion_planning.launch.py'
+            )
+        )
+    )
+
+    planner_lat_lane_follow_ldm = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('plan_lat_lane_follow_ldm'),
+                'launch',
+                'plan_lat_lane_follow_ldm.launch.py'
+            )
+        )
+    )
+
+    vehicle_control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(
+                get_package_share_directory('ctrl_vehicle_control'),
+                'launch',
+                'ctrl_vehicle_control.launch.py')
+        )
+    )
+
+    return LaunchDescription([
+        novatel_gps,
+        static_tf,
+        vehicle_can,
+
+        sensor_abstraction,
+        environmental_fusion,
+        behavior_planning,
+        motion_planning,
+        vehicle_control,
+
+        planner_lat_lane_follow_ldm
+    ])
