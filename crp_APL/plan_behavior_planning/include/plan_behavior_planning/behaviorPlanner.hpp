d882b9d738b0c45dc095fdc6273bcac0e9a32472
AnonymDavid <jodavid512@gmail.com>
2024-11-07 11:02:29 +0100

    Moved behavior max speed to behaviorPlanner

--- crp_APL/plan_behavior_planning/include/plan_behavior_planning/behaviorPlanner.hpp
+++ crp_APL/plan_behavior_planning/include/plan_behavior_planning/behaviorPlanner.hpp
@@ -5,6 +5,7 @@
 #include <crp_msgs/msg/scenario.hpp>
 #include <crp_msgs/msg/ego.hpp>
 #include <crp_msgs/msg/target_space.hpp>
+#include <crp_msgs/msg/behavior.hpp>
 #include <autoware_planning_msgs/msg/lanelet_route.hpp>
 #include <tier4_planning_msgs/msg/scenario.hpp>
 
@@ -23,17 +24,20 @@ private:
     void routeCallback(const autoware_planning_msgs::msg::LaneletRoute::SharedPtr msg);
     void scenarioCallback(const crp_msgs::msg::Scenario::SharedPtr msg);
     void egoCallback(const crp_msgs::msg::Ego::SharedPtr msg);
+    void behaviorCallback(const crp_msgs::msg::Behavior::SharedPtr msg);
     void loop();
 
     rclcpp::Subscription<autoware_planning_msgs::msg::LaneletRoute>::SharedPtr m_sub_route;
     rclcpp::Subscription<crp_msgs::msg::Scenario>::SharedPtr                   m_sub_scenario;
     rclcpp::Subscription<crp_msgs::msg::Ego>::SharedPtr                        m_sub_ego;
+    rclcpp::Subscription<crp_msgs::msg::Behavior>::SharedPtr                   m_sub_behavior_;
 
     rclcpp::Publisher<tier4_planning_msgs::msg::Scenario>::SharedPtr m_pub_strategy;
     rclcpp::Publisher<crp_msgs::msg::TargetSpace>::SharedPtr         m_pub_target_space;
 
     rclcpp::TimerBase::SharedPtr timer_;
     
+    float m_maximum_speed;
 };
 
 } // namespace apl
