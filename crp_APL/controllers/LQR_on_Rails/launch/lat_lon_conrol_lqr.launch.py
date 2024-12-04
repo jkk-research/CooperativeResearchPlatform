77a55911eccb22b92bd1478deefc7a85d1f2bd53
mesmatyi <mesicsmatyas@gmail.com>
2024-12-04 10:48:24 +0100

    Rename folder and etc..

---
+++ crp_APL/controllers/LQR_on_Rails/launch/lat_lon_conrol_lqr.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lat_lon_conrol_lqr',
+            executable='lqr_controller.py',
+            name='lqr_controller',
+            output='screen',
+            parameters=[{
+                'dt': 0.03,
+                'wheel_base': 1.9,
+                'max_steer_tire_angle': 0.436332,
+                'Q': [0.001, 0.0, 0.001, 0, 0.8], # Q matrix diagonal elements 
+                'R': [30.0, 2.0], # R matrix diagonal elements
+            }]
+        )
+    ])
