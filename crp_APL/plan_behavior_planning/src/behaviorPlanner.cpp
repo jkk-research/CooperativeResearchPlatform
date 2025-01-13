8e7d8113c2099c55ad49ceca7ec1fd1ec17952be
gfigneczi1 <gergo.igneczi@arrabonaracing.hu>
2025-01-13 07:51:53 +0100

    deletion of unused comments

--- crp_APL/plan_behavior_planning/src/behaviorPlanner.cpp
+++ crp_APL/plan_behavior_planning/src/behaviorPlanner.cpp
@@ -36,12 +36,6 @@ void crp::apl::BehaviorPlanner::scenarioCallback(const crp_msgs::msg::Scenario::
         targetSpaceMsg.path = msg->paths[0];
     }
 
-
-    //for (tier4_planning_msgs::msg::PathPointWithLaneId & pathPoint : targetSpaceMsg.path.path.points)
-    //{
-    //    pathPoint.point.longitudinal_velocity_mps = std::min(m_maximum_speed, pathPoint.point.longitudinal_velocity_mps);
-    //}
-
     targetSpaceMsg.path.traffic_rules.maximum_speed = m_maximum_speed;
 
     targetSpaceMsg.free_space = msg->free_space;
